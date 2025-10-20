e86c2d48c671f64977141f75c85b145f6a1e58c6
ManhCuong200 <tmanhcuong2k4@gmail.com>
2025-10-20 20:43:43 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,26 @@ import ToDoItem from "./components/ToDoItem";
 import ToDoHeader from "./components/ToDoHeader";
 
 function App() {
+  const [listTasks, setlistTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [
+      {
+        id: 1,
+        title: "Learn React components",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Master Javascript props",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Master Nodejs props",
+        completed: true,
+      },
+    ]
+  );
   const [isModalOpen, setIsModalOpen] = useState(false);
-
   const handleOpenModal = () => {
     setIsModalOpen(true);
   };
@@ -13,13 +31,34 @@ function App() {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
+  const handleClick = (id) => {
+    setlistTasks(
+      listTasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+  const totalCompeleted = listTasks.filter((task) => task.completed).length;
+  const totalTask = listTasks.length;
+  const percentCompeleted = ((totalCompeleted / totalTask) * 100).toFixed(0);
+  const totalInprogress = totalTask - totalCompeleted;
 
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-pink-200 via-purple-200 to-blue-200 p-6">
         <div className="max-w-2xl mx-auto">
-          <ToDoHeader onAddTask={handleOpenModal} />
-          <ToDoItem />
+          <ToDoHeader
+            totalInprogress={totalInprogress}
+            percentCompeleted={percentCompeleted}
+            totalTasks={totalTask}
+            totalCompeleted={totalCompeleted}
+            onOpenTask={handleOpenModal}
+          />
+          <div className="grid gap-4">
+            {listTasks.map((task, index) => (
+              <ToDoItem key={index} task={task} handleClick={handleClick} />
+            ))}
+          </div>
         </div>
       </div>
       <AddTaskModal open={isModalOpen} onOpenChange={handleCloseModal} />
--- src/components/ToDoHeader/index.jsx
+++ src/components/ToDoHeader/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const TodoHeader = ({ onAddTask }) => {
+const TodoHeader = ({
+  onOpenTask,
+  totalCompeleted,
+  totalTasks,
+  percentCompeleted,
+  totalInprogress,
+}) => {
   return (
     <>
       <div className="bg-yellow-300 border-4 border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] rounded-none p-8 mb-8">
@@ -15,24 +21,38 @@ const TodoHeader = ({ onAddTask }) => {
             <h2 className="text-2xl font-black text-black mb-4">
               MISSION PROGRESS
             </h2>
-            <div className="text-3xl font-black text-black mb-4">
-              <span>You've completed 1/2 tasks</span>
+            <div
+              className={`text-3xl font-black mb-4 ${
+                totalCompeleted === totalTasks
+                  ? "text-green-600"
+                  : "text-black "
+              }`}
+            >
+              <span>
+                {totalCompeleted ===
+                  `You've completed ${totalCompeleted}/${totalTasks} tasks`}
+                {totalCompeleted === totalTasks ? "ALL MISSIONS COMPLETE!" : ""}
+              </span>
             </div>
             <div className="bg-white border-3 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] rounded-none h-8 overflow-hidden">
               <div
                 className="bg-green-400 h-full border-r-3 border-black transition-all duration-500 flex items-center justify-center"
-                style={{ width: "50%" }}
+                style={{ width: `${percentCompeleted}%` }}
               >
-                <span className="text-black font-black text-sm">50%</span>
+                <span className="text-black font-black text-sm">
+                  {percentCompeleted}%
+                </span>
               </div>
             </div>
             <p className="text-lg font-bold text-black mt-4 ">
-              1 missions remaining
+              {totalInprogress === 0
+                ? "Ready for launch!"
+                : totalInprogress + " missions remaining"}
             </p>
           </div>
         </div>
-        <button 
-          onClick={onAddTask}
+        <button
+          onClick={onOpenTask}
           className="bg-green-300 w-full border-4 border-black shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] rounded-none p-4 inline-block hover:translate-x-1 hover:translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200"
         >
           <p className="text-xl font-black text-black">Add New Task</p>
@@ -41,10 +61,10 @@ const TodoHeader = ({ onAddTask }) => {
           <button className="px-4 w-full py-2 border-3 border-black hover:translate-x-1 hover:translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 shadow-[3px_3px_0px_#000] bg-yellow-300">
             All
           </button>
-          <button class="px-4 w-full bg-green-300 py-2 border-3 border-black hover:translate-x-1 hover:translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 shadow-[3px_3px_0px_#000]  bg-gray-200">
+          <button className="px-4 w-full bg-green-300 py-2 border-3 border-black hover:translate-x-1 hover:translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 shadow-[3px_3px_0px_#000]  bg-gray-200">
             Active
           </button>
-          <button class="px-4 bg-orange-300 py-2 w-full border-3 border-black hover:translate-x-1 hover:translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 shadow-[3px_3px_0px_#000] bg-gray-200">
+          <button className="px-4 bg-orange-300 py-2 w-full border-3 border-black hover:translate-x-1 hover:translate-y-1 hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 shadow-[3px_3px_0px_#000] bg-gray-200">
             Completed
           </button>
         </div>
